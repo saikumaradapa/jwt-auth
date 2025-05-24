e0969ced24d0420ec79671ed60e4288d107a7f76
saikumaradapa <saikumaradapa3266@gmail.com>
2025-05-24 21:30:05 +0530

    add server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/saikumaradapa/jwt-auth
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/register", handlers.Register)
+	http.HandleFunc("/login", handlers.Login)
+	http.HandleFunc("/users", handlers.GetUsers)
+	http.HandleFunc("/protected", middleware.JWTMiddleware(http.HandlerFunc(handlers.Protected)))
+
+	fmt.Println("server running on http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
